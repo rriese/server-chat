d21ab47081e6d27e841e802b1be6e92d190d1470
Rafael Riese <rafael.riese@catolicasc.org.br>
2018-11-07 09:50:54 -0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     io = require('socket.io')(server),
-    server = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+    server = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 // Base de dados das mensagens
 var messages = [];
@@ -32,4 +32,4 @@ io.on('connection', socket => {
     });
 });
 
-server.listen(port);
+server.listen(port, server);
